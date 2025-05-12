081ed06ba33a32a76c41bcdd5d6b806fcbe80c7b
Sam Allen <samallen659@gmail.com>
2025-05-12 20:37:44 +0100

    adds additional formating for cpu usage

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+echo "CPU USAGE"
 CPU_FREE=$(top -bn1 | grep "Cpu(s)" | tr -s " " | cut -d " " -f 8)
-echo $CPU_FREE
+echo "$CPU_FREE%"
+echo ""
+
+MEMORY=$(free -m | grep Mem | tr -s " ")
