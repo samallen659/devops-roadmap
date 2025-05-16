78b56dbb4df74a19d41be1890e8c046b3da4e657
Sam Allen <samallen659@gmail.com>
2025-05-16 20:18:49 +0100

    adds Disk output

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -8,8 +8,17 @@ echo
 MEMORY=$(free -m | grep "Mem" | tr -s " ")
 TOTAL_MEMORY=$(echo "$MEMORY" | cut -d ' ' -f 2)
 USED_MEMORY=$(echo "$MEMORY" | cut -d ' ' -f 3)
-USED_PERCENT=$(echo "scale=3;(($USED_MEMORY / $TOTAL_MEMORY) * 100)" | bc | sed 's/..$//')
+MEMORY_PERCENT=$(echo "scale=3;(($USED_MEMORY / $TOTAL_MEMORY) * 100)" | bc | sed 's/..$//')
 
 echo "MEMORY USAGE"
-echo -e "TOTAL: $TOTAL_MEMORY\tUSED_MEMORY: $USED_MEMORY\tUSED_PERCENT: $USED_PERCENT%"
+echo -e "TOTAL: $TOTAL_MEMORY\tUSED: $USED_MEMORY\tUSED_PERCENT: $MEMORY_PERCENT%"
+echo
+
+DISK_USAGE=$(df -h | grep "/" -w)
+TOTAL_DISK=$(echo $DISK_USAGE | cut -d " " -f 2)
+USED_DISK=$(echo $DISK_USAGE | cut -d " " -f 3)
+DISK_PERCENT=$(echo $DISK_USAGE | cut -d " " -f 5)
+
+echo "DISK USAGE"
+echo -e "TOTAL: $TOTAL_DISK\tUSED: $USED_DISK\tUSED_PERCENT: $DISK_PERCENT"
 echo
