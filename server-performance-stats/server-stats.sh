0535444934a27d521ee55e9181aa32b78dfdb649
Sam Allen <samallen659@gmail.com>
2025-05-16 20:24:00 +0100

    amends title's to lowercase

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "CPU USAGE"
+echo "CPU Usage"
 CPU_FREE=$(top -bn1 | grep "Cpu(s)" | tr -s " " | cut -d " " -f 8)
 echo "$CPU_FREE%"
 echo
@@ -10,7 +10,7 @@ TOTAL_MEMORY=$(echo "$MEMORY" | cut -d ' ' -f 2)
 USED_MEMORY=$(echo "$MEMORY" | cut -d ' ' -f 3)
 MEMORY_PERCENT=$(echo "scale=3;(($USED_MEMORY / $TOTAL_MEMORY) * 100)" | bc | sed 's/..$//')
 
-echo "MEMORY USAGE"
+echo "Memory Usage"
 echo -e "TOTAL: $TOTAL_MEMORY\tUSED: $USED_MEMORY\tUSED_PERCENT: $MEMORY_PERCENT%"
 echo
 
@@ -19,6 +19,6 @@ TOTAL_DISK=$(echo $DISK_USAGE | cut -d " " -f 2)
 USED_DISK=$(echo $DISK_USAGE | cut -d " " -f 3)
 DISK_PERCENT=$(echo $DISK_USAGE | cut -d " " -f 5)
 
-echo "DISK USAGE"
+echo "Disk Usage"
 echo -e "TOTAL: $TOTAL_DISK\tUSED: $USED_DISK\tUSED_PERCENT: $DISK_PERCENT"
 echo
