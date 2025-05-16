3ec76ebe6f34aed4dba1b1b37d62c8444a74060c
Sam Allen <samallen659@gmail.com>
2025-05-16 19:56:59 +0100

    adds Memory output

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -5,4 +5,11 @@ CPU_FREE=$(top -bn1 | grep "Cpu(s)" | tr -s " " | cut -d " " -f 8)
 echo "$CPU_FREE%"
 echo ""
 
-MEMORY=$(free -m | grep Mem | tr -s " ")
+MEMORY=$(free -m | grep "Mem" | tr -s " ")
+TOTAL_MEMORY=$(echo "$MEMORY" | cut -d ' ' -f 2)
+USED_MEMORY=$(echo "$MEMORY" | cut -d ' ' -f 3)
+USED_PERCENT=$(echo "scale=3;(($USED_MEMORY / $TOTAL_MEMORY) * 100)" | bc | sed 's/..$//')
+
+echo "MEMORY USAGE"
+echo -e "TOTAL: $TOTAL_MEMORY\tUSED_MEMORY: $USED_MEMORY\tUSED_PERCENT: $USED_PERCENT%"
+echo ""
