995fa00dc533c9305be174631658b077bad79635
Sam Allen <samallen659@gmail.com>
2025-05-18 16:18:16 +0100

    adds log archive tool

--- log-archive-tool/log-archive.sh
+++ log-archive-tool/log-archive.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+if [ "$1" == '' ]; then
+  echo ERROR: Path to logs required
+  exit 1
+fi
+
+DATE_TIME=$(date +"%Y%m%d_%H%M%S")
+FILE_NAME="logs_archive_$DATE_TIME.tar.gz"
+
+tar czf "$FILE_NAME" "$1"
+
+mv "$FILE_NAME" /var/log/"$FILE_NAME"
